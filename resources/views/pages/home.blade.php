89c78dd873f7c2868ced30719ddc820e3851d6a1
DeftCorgi <toanau123@gmail.com>
2017-08-13 16:27:42 +1000

    jumbotron

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts/main')
 
 @section('content')
-<div class="page-header">
-  <h1>ITS ticketing</h1>
+<div class="jumbotron text-center">
+  <h1>ITS Ticketing</h1>
+  <p>Fast IT support for RMIT students</p>
+  <button class="btn btn-primary btn-lg">Create Ticket</button>
+  <button class="btn btn-primary btn-lg">View Tickets</button>
 </div>
 @endsection
