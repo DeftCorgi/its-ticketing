e5821e735efa1b77d0d9ff117b80d488199fe3d9
DeftCorgi <toanau123@gmail.com>
2017-08-13 16:21:30 +1000

    method stubs

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class TicketsController extends Controller
 {
-    //
+    public function index() {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function show() {
+
+    }
+
+    public function destroy() {
+        
+    }
 }
